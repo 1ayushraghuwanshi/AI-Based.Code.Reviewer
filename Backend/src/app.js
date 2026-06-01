d26da18780f15dc7ee21e0bfd9807050f5d29f68
1ayushraghuwanshi <ayushraghu1c84@gmail.com>
2026-06-01 13:36:56 +0530

    fix: add missing semicolons to resolve ESLint errors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const aiRoutes = require('./routes/ai.route')
-const cors = require('cors')
+const aiRoutes = require('./routes/ai.route');
+const cors = require('cors');
 
 
 const app = express();
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+    res.send("Hello World");
+});
 
 app.use('/ai', aiRoutes);
 
